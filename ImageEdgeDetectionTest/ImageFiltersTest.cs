9e0f864b058f37ed6f91a340550c9068d4b6d0d4
Alicia <alicia.zangger@gmail.com>
2018-10-06 11:27:32 +0200

    Added test class EdgeDetectionTest
    Created test for image filter Black&White

---
+++ ImageEdgeDetectionTest/ImageFiltersTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using ImageEdgeDetection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImageEdgeDetectionTest
+{
+    [TestClass]
+    public class ImageFiltersTest
+    {
+        /*Filter tested : black & white from ImageFilters class*/
+        [TestMethod]
+        public void BlackWhiteTest()
+        {
+            int width = 100;
+            int height = 100;
+            int redValue = 120;
+            int red;
+            int greenValue = 90;
+            int green;
+            int blueValue = 150;
+            int blue;
+            int rgb = (int)((redValue + greenValue + blueValue) / 3);
+            Color color;
+
+            Bitmap TestImg = new Bitmap(width, height);
+            Bitmap Result;
+
+            for (int y = 0; y < TestImg.Height; y++)
+                for (int x = 0; x < TestImg.Width; x++)
+                {
+                    TestImg.SetPixel(x, y, Color.FromArgb(redValue, greenValue, blueValue));
+                }
+
+            Result = ImageFilters.BlackWhite(TestImg);
+
+            for (int y = 0; y < Result.Height; y++)
+                for (int x = 0; x < Result.Width; x++)
+                {
+                    color = Result.GetPixel(x, y);
+                    red = (int) color.R;
+                    green = (int) color.G;
+                    blue = (int) color.B;
+                    Assert.AreEqual(red, rgb);
+                    Assert.AreEqual(green, rgb);
+                    Assert.AreEqual(blue, rgb);
+                }
+        }
+    }
+}
