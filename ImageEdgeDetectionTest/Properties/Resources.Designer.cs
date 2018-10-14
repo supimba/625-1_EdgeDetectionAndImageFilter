a1f42a5b429b29e0b799253bd500bd7cf431d62c
Supimba <batterteam@gmail.com>
2018-10-14 21:50:41 +0200

    Add cherries after merge because conflict

--- ImageEdgeDetectionTest/Properties/Resources.Designer.cs
+++ ImageEdgeDetectionTest/Properties/Resources.Designer.cs
@@ -63,6 +63,26 @@ namespace ImageEdgeDetectionTest.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap cherry {
+            get {
+                object obj = ResourceManager.GetObject("cherry", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap cherry_sobel {
+            get {
+                object obj = ResourceManager.GetObject("cherry_sobel", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap unitTestPropImage1 {
             get {
                 object obj = ResourceManager.GetObject("unitTestPropImage1", resourceCulture);
